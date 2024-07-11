81c46a08f717f5e899bc6dbfcfd7bcfc6ccb4ef0
Omar Qazi <omar@smick.com>
2024-07-11 15:28:38 -0700

    Initial Commit

---
+++ Rando/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rando
+//
+//  Created by Omar Qazi on 7/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rando/RandoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandoApp.swift
+//  Rando
+//
+//  Created by Omar Qazi on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct RandoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
